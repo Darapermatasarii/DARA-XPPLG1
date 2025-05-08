fb35084fd073eddcd3edef3ccf0cfff2befb0de5
Darapermatasarii <darapermatasari21@gmail.com>
2025-05-08 15:27:52 +0700

    tugas operator

---
+++ operator.py
@@ -0,0 +1,23 @@
+# Dara sedang menghitung nilai ujian matematika
+nama = "Dara"
+nilai_1 = 85
+nilai_2 = 90
+
+# Operasi Aritmatika
+jumlah = nilai_1 + nilai_2
+rata_rata = (nilai_1 + nilai_2) / 2
+selisih = nilai_2 - nilai_1
+perkalian = nilai_1 * 2
+pembagian = nilai_2 / 2
+modulus = nilai_2 % nilai_1  # Sisa bagi
+pangkat = nilai_1 ** 2
+
+# Output hasil perhitungan
+print(f"Nama: {nama}")
+print(f"Jumlah nilai: {jumlah}")
+print(f"Rata-rata nilai: {rata_rata}")
+print(f"Selisih nilai: {selisih}")
+print(f"{nilai_1} dikali 2 = {perkalian}")
+print(f"{nilai_2} dibagi 2 = {pembagian}")
+print(f"Sisa bagi {nilai_2} % {nilai_1} = {modulus}")
+print(f"{nilai_1} pangkat 2 = {pangkat}")
