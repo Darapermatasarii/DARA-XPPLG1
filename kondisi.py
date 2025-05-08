e0bb04c8652cb1f720f7a4c54b9be7cec7a0f678
Darapermatasarii <darapermatasari21@gmail.com>
2025-05-08 15:26:03 +0700

    tugas kondisi

---
+++ kondisi.py
@@ -0,0 +1,8 @@
+nama = input("nama anda: ")
+
+if nama == "Dara":
+    print("Selamat datang, Dara! Anda memiliki akses penuh.")
+elif nama == "dara":
+    print("Apakah Anda lupa menggunakan huruf kapital? Harusnya 'Dara'.")
+else:
+    print("Maaf, akses hanya diberikan kepada Dara.")
